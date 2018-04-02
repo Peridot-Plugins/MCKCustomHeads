4af1b9b3678078ee72230ee8d589f898ca938c1f
McKoxu <blackxxoj@gmail.com>
2018-04-02 15:20:35 +0200

    1.1

---
+++ src/me/mckoxu/mckcustomheads/commands/CustomHead.java
@@ -0,0 +1,113 @@
+package me.mckoxu.mckcustomheads.commands;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import me.mckoxu.mckcustomheads.MCKCustomHeads;
+import me.mckoxu.mckcustomheads.methods.HeadsCreating;
+import me.mckoxu.mckcustomheads.objects.Heads;
+
+public class CustomHead implements CommandExecutor, Listener{
+	
+	FileConfiguration config = MCKCustomHeads.getInst().getConfig();
+	
+	public static ItemStack customp = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);{
+		ItemMeta im = customp.getItemMeta();
+		ArrayList<String> lore = new ArrayList<String>();
+		for (String s : config.getStringList("mainpanel.items.customplayerskull.lore")) {
+			lore.add(ChatColor.translateAlternateColorCodes('&', s));
+		}
+		im.setLore(lore);
+		im.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("mainpanel.items.customplayerskull.name")));
+		customp.setItemMeta(im);
+	}
+	public static ItemStack customs = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);{
+		ItemMeta im = HeadsCreating.getHead("decorations-monitor").getItemMeta();
+		ArrayList<String> lore = new ArrayList<String>();
+		for (String s : config.getStringList("mainpanel.items.customskull.lore")) {
+			lore.add(ChatColor.translateAlternateColorCodes('&', s));
+		}
+		im.setLore(lore);
+		im.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("mainpanel.items.customskull.name")));
+		customs.setItemMeta(im);
+	} 
+	public static ItemStack skullistitem = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);{
+		ItemMeta im = HeadsCreating.getHead("food-bread").getItemMeta();
+		ArrayList<String> lore = new ArrayList<String>();
+		int amount = 0;
+		for (Heads head : Heads.values()){
+			amount++;
+		}
+		for (String s : config.getStringList("mainpanel.items.skullist.lore")) {
+			lore.add(ChatColor.translateAlternateColorCodes('&', s.replace("{AMOUNT}", String.valueOf(amount))));
+		}
+		im.setLore(lore);
+		im.setDisplayName(ChatColor.translateAlternateColorCodes('&', config.getString("mainpanel.items.skullist.name")).replace("{AMOUNT}", String.valueOf(amount)));
+		skullistitem.setItemMeta(im);
+	}
+	
+	public static Inventory maininv;
+	public CustomHead(){
+		maininv = Bukkit.createInventory(null, 27, ChatColor.translateAlternateColorCodes('&', config.getString("mainpanel.name")));
+	}
+	@Override
+	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("customhead")){
+			if(s instanceof Player){
+				Inventory inv = maininv;
+				Player p = (Player) s;
+				if(p.hasPermission("mck.headsmenu")){
+					if(args.length >= 1){
+						if(args[0].equalsIgnoreCase("give")){
+							if(p.hasPermission("mck.headgive")){					
+								if(args.length >= 2){
+									String msg = "";
+									for(int i = 1; i<args.length; i++){
+										msg += " " + args[i];
+									}
+									p.getInventory().addItem(HeadsCreating.createCustomSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUv"+msg));
+								} else{
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.correctusage")));
+									p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.texture")));
+								}
+							}else{
+								p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("error.noperm")));
+							}
+						} else{
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.correctusage")));
+							p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.notrequired")));
+						}
+					} else{
+						p.openInventory(inv);
+						for (int i = 0; i < 27; i++) {
+							inv.setItem(i, new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 15));
+						}
+						inv.setItem(13, skullistitem);
+						inv.setItem(11, customs);
+						inv.setItem(15, customp); 
+					}
+				} else{
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.noperm")));
+				}
+			} else{
+				s.sendMessage("[MCKCustomHeads] This command can be used only by players !");
+			}
+		}
+		return false;
+	}
+	
+	
+	
+}
---
+++ src/me/mckoxu/mckcustomheads/listeners/InventoryClickListener.java
@@ -0,0 +1,84 @@
+package me.mckoxu.mckcustomheads.listeners;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import com.cloutteam.samjakob.gui.ItemBuilder;
+import com.cloutteam.samjakob.gui.buttons.GUIButton;
+import com.cloutteam.samjakob.gui.types.PaginatedGUI;
+
+import me.mckoxu.mckcustomheads.MCKCustomHeads;
+import me.mckoxu.mckcustomheads.commands.CustomHead;
+import me.mckoxu.mckcustomheads.objects.Heads;
+import net.wesjd.anvilgui.AnvilGUI;
+
+public class InventoryClickListener implements Listener{
+	
+	FileConfiguration config = MCKCustomHeads.getInst().getConfig();
+	PaginatedGUI skullist = new PaginatedGUI(ChatColor.translateAlternateColorCodes('&', config.getString("skullistpanel.name")));
+	
+	@EventHandler
+	public void onClick(InventoryClickEvent e){
+		Inventory i = e.getInventory();
+		if(i.equals(CustomHead.maininv)){
+			Player p = (Player) e.getWhoClicked();
+			ItemStack is = e.getCurrentItem();
+			if(is.equals(CustomHead.customp)){
+				new AnvilGUI(MCKCustomHeads.getInst(), p, config.getString("customplayerhead.maintext"), (player, reply) -> {
+				    if (reply != null) {
+				        ItemStack skull = new ItemStack(Material.SKULL_ITEM,1,(short)3);
+                        SkullMeta im = (SkullMeta)skull.getItemMeta();
+                        im.setOwner(reply);
+                        skull.setItemMeta(im);
+						player.getInventory().addItem(skull);
+				        return null;
+				    }
+				    return "Incorrect.";
+				});
+			} else if(is.equals(CustomHead.skullistitem)){
+				ArrayList<String> lore = new ArrayList<String>();
+				for (String s : config.getStringList("skullistpanel.items.back.lore")) {
+					lore.add(ChatColor.translateAlternateColorCodes('&', s));
+				}
+				int amount = 0;
+				for (Heads head : Heads.values()){
+					GUIButton skull = new GUIButton(
+							ItemBuilder.start(Material.SKULL_ITEM).name("Head").data((short)3).itemmeta(head.getItemStack().getItemMeta()).build()
+					);
+					GUIButton back = new GUIButton(
+							ItemBuilder.start(Material.EMPTY_MAP).name(config.getString(ChatColor.translateAlternateColorCodes('&', "skullistpanel.items.back.name"))).lore(lore).build()
+					);
+					skull.setListener(event -> {
+						if(event.getCurrentItem().getType().equals(Material.SKULL_ITEM)){
+							 p.getInventory().addItem(event.getCurrentItem());
+							 event.setCancelled(true);
+						}
+					});
+					back.setListener(event -> {
+					    p.openInventory(CustomHead.maininv);
+					    event.setCancelled(true);
+					});
+					skullist.setToolbarItem(0, back);
+					skullist.setButton(amount, skull);
+					amount++;
+				}
+				p.openInventory(skullist.getInventory());	
+			} else{
+				p.openInventory(CustomHead.maininv);
+				e.setCancelled(true);
+			}
+		} else{
+			return;
+		}
+	}
+}
